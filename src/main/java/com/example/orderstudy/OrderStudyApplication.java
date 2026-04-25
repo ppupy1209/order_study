8fe00568aaca240210835aa90d9b4f61ee0801d7
kimyeonwoo <ppupy1209@naver.com>
2026-04-26 08:35:21 +0900

    init: initialize spring boot project

---
+++ src/main/java/com/example/orderstudy/OrderStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderStudyApplication.class, args);
+	}
+
+}
